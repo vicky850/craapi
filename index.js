26b44bdb205472345bde8ef0590ec7683f4fbecb
vicky850 <40638484+vicky850@users.noreply.github.com>
2019-01-28 17:01:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "Welcome123#",
+//     database: 'cradb'           //cradb
+// })
+
 const express = require('express')
 const cors = require('cors')
 const mysql = require('mysql')
 var bodyParser = require('body-parser')
 var path = require('path');
-const port = process.env.PORT || 3000;
 
 const app = express()
 app.use(cors());
@@ -17,13 +23,21 @@ app.use(function(req, res, next) {
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-var db_config = {
+
+//   var db_config = {
+//     host: "localhost",
+//     user: "root",
+//     password: "Welcome123#",
+//     database: 'cradb'           //cradb    
+//   };
+
+  var db_config = {
     host: "182.50.133.77",
     user: "craadvocate",
     password: "Cra@2019",
     database: 'cradb'     
   };
-  
+
   var connection;
   
   function handleDisconnect() {
@@ -55,7 +69,7 @@ var db_config = {
 app.get("/api/clients", function(req, res) {
     const createdby=req.query.createdby;
     const clientinfo=req.query.clientinfo;
-    const SELECT_ALL_CLIENTS = `SELECT * FROM clientmaster where createdby in ('${createdby}', '') and clientinfo=${clientinfo} order by 'firmname'`
+    const SELECT_ALL_CLIENTS = `SELECT * FROM clientmaster order by 'firmname'`
 
     connection.query(SELECT_ALL_CLIENTS, (err, resultados) => {
         if (err) {
@@ -66,6 +80,19 @@ app.get("/api/clients", function(req, res) {
   });
 });
 
+app.get("/api/clientsbyid", function(req, res) {
+    const createdby=req.query.createdby;
+    const clientinfo=req.query.clientinfo;
+    const SELECT_ALL_CLIENTS = `SELECT * FROM clientmaster where createdby in ('${createdby}', '') and clientinfo=${clientinfo} order by 'firmname'`
+
+    connection.query(SELECT_ALL_CLIENTS, (err, resultados) => {
+        if (err) {
+          handleError(res, err.message, "Failed to get contacts.");
+        } else {
+          res.status(200).json(resultados);
+        }
+  });
+});
 
 app.get('/api/allactiveclients', (req, res) => {
     const clientinfo=req.query.clientinfo;
@@ -96,7 +123,7 @@ app.get('/api/clientInfo', (req, res) => {
 //get call
 app.get('/api/clientFee', (req, res) => {
     const filenumber=req.query.filenumber;
-    const SELECT_ALL_CLIENTS = `SELECT * FROM efiling where filenumber = '${filenumber}'`
+    const SELECT_ALL_CLIENTS = `CALL sp_get_all('${filenumber}')`
     connection.query(SELECT_ALL_CLIENTS, (err, resultados) => {
         if (err) {
             return res.send(err)
@@ -106,6 +133,77 @@ app.get('/api/clientFee', (req, res) => {
     })
 })
 
+
+//get call
+app.get('/api/clientEFile', (req, res) => {
+    const filenumber=req.query.filenumber;
+    const SELECT_ALL_CLIENTS = `select Tb1.filenumber,Tb1.firmname,Tb1.year,Tb1.month,Tb1.returntype,Tb1.dateofreceipt,Tb1.comunicationmode,Tb1.personincaselatereturn,Tb1.typeofpendancy,Tb1.fillingdate,Tb1.filledby,Tb1.efillcategory,Tb1.gstr3bturnover,Tb1.correspondacedatepending,Tb1.personname,Tb1.feedue,Tb1.id,Tb2.receivedamount, Tb2.receiptnumber,Tb2.recieptdate from (select * from efiling where filenumber='${filenumber}') as Tb1
+    left join (select filenumber, year, month, sum(receivedamount)receivedamount, receiptnumber, recieptdate from clientfee group by filenumber,year,month) Tb2 on Tb1.filenumber=Tb2.filenumber and Tb1.year=Tb2.year and Tb1.month=Tb2.month`
+    connection.query(SELECT_ALL_CLIENTS, (err, resultados) => {
+        if (err) {
+            return res.send(err)
+        } else {
+            return res.send(resultados);
+        }
+    })
+})
+
+//create call
+app.put('/api/clients/addFee', (req, res) => {
+    const body = req.body;
+    let columns=Object.keys(req.body[0]);
+
+    const serialize = body.map(data=>columns.map(key => `'${decodeURIComponent(data[key])}'`).join(','));
+    let newArr=[];
+    for(let i=0; i<serialize.length; i++){
+        newArr.push(`(${serialize[i]})`);
+    }
+    let finalValues = newArr.join(',');
+    const INSERT_USER_QUERY = `Delete from efiling where filenumber='${body[0].filenumber}' INSERT INTO efiling(${columns.toString()}) VALUES ${finalValues}`
+    connection.query(INSERT_USER_QUERY, (err, resultados) => {
+        if (err) {
+            return res.send(err)
+        } else {
+            return res.send('insert successful')
+        }
+    })
+})
+
+//create call
+app.put('/api/clients/addEFile', (req, res) => {
+    const body = req.body;
+    let columns=Object.keys(req.body[0]);
+
+    const serialize = body.map(data=>columns.map(key => `'${decodeURIComponent(data[key])}'`).join(','));
+    let newArr=[];
+    for(let i=0; i<serialize.length; i++){
+        //newArr.push(`${serialize[i]}`);
+        newArr.push(`(${serialize[i]})`);
+    }
+    let finalValues = newArr.join(',');
+    const INSERT_USER_QUERY = `Delete from efiling where filenumber='${body[0].filenumber}'; INSERT INTO efiling(${columns.toString()}) VALUES ${finalValues}`
+    //const INSERT_USER_QUERY = `CALL sp_insertEfile(${finalValues})`
+    connection.query(INSERT_USER_QUERY, (err, resultados) => {
+        if (err) {
+            return res.send(err)
+        } else {
+            return res.send('insert successful')
+        }
+    })
+})
+
+//get call
+app.get('/api/clientEFile/add', (req, res) => {
+    const filenumber=req.query.filenumber;
+    const SELECT_ALL_CLIENTS = `SELECT * FROM efiling where filenumber = '${filenumber}'`
+    connection.query(SELECT_ALL_CLIENTS, (err, resultados) => {
+        if (err) {
+            return res.send(err)
+        } else {
+            return res.send(resultados);
+        }
+    })
+})
 
 //create call
 app.put('/api/clients/add', (req, res) => {
@@ -137,26 +235,7 @@ app.put('/api/clients/addInfo', (req, res) => {
     })
 })
 
-//create call
-app.put('/api/clients/addFee', (req, res) => {
-    const body = req.body;
-    let columns=Object.keys(req.body[0]);
 
-    const serialize = body.map(data=>columns.map(key => `'${decodeURIComponent(data[key])}'`).join(','));
-    let newArr=[];
-    for(let i=0; i<serialize.length; i++){
-        newArr.push(`(${serialize[i]})`);
-    }
-    let finalValues = newArr.join(',');
-    const INSERT_USER_QUERY = `Delete from efiling where filenumber=${body[0].filenumber} INSERT INTO efiling(${columns.toString()}) VALUES ${finalValues}`
-    connection.query(INSERT_USER_QUERY, (err, resultados) => {
-        if (err) {
-            return res.send(err)
-        } else {
-            return res.send('insert successful')
-        }
-    })
-})
 
 //update call
 app.post('/api/clients/updateclientInfo', (req, res) => {
@@ -172,7 +251,7 @@ app.post('/api/clients/updateclientInfo', (req, res) => {
     })
 })
 
-app.listen(port, () => {
-    console.log(`server started at port: ${port}`)
+app.listen(7000, () => {
+    console.log('server started at port 7000')
 })
 module.exports={app};
